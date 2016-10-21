fd35feea81754143650d6cd30d10514b42dca494
Andrew Arrow <andrew@0x7a69.net>
2016-10-21 12:52:03 +0000

    w

--- parse/parse.go
+++ parse/parse.go
@@ -27,7 +27,7 @@ func readJava(path, orig string) {
 	for _, toke := range tokens {
 		jpackage = append(jpackage, toke)
 	}
-	jpackage = jpackage[0 : len(jpackage)-2]
+	jpackage = jpackage[0 : len(jpackage)-1]
 	gopackage := make([]string, 0)
 	flip := false
 	for _, name := range jpackage {
@@ -41,9 +41,20 @@ func readJava(path, orig string) {
 	//fmt.Println(gopackage)
 	if len(gopackage) > 2 {
 		first := strings.Join(gopackage[1:3], "_")
-		fmt.Println(first)
+		//fmt.Println(first)
 		dirname := fmt.Sprintf("%s_go/%s", orig, first)
 		os.Mkdir(dirname, 0777)
+
+		endparts := strings.Split(strings.ToLower(tokens[len(tokens)-1]), ".")
+		endpart := endparts[0]
+
+		rest := gopackage[2:len(gopackage)]
+		reststr := ""
+		if len(rest) > 1 {
+			reststr = strings.Join(rest[1:len(rest)], "_")
+		}
+		filename := fmt.Sprintf("%s_go/%s/%s", orig, first, reststr+"_"+endpart+".go")
+		fmt.Println(filename)
 	}
 
 	for _, _ = range lines {
